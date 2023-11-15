da0f405992a1a116e73b540f63882b0a2df4abbb
tugberk963 <tugberk963@gmail.com>
2023-11-15 19:32:58 +0300

    created tweet button and tweet listing

--- twitter_clone_flutter/lib/main.dart
+++ twitter_clone_flutter/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:twitter_clone_client/twitter_clone_client.dart';
 import 'package:flutter/material.dart';
 import 'package:serverpod_flutter/serverpod_flutter.dart';
+import 'package:twitter_clone_flutter/post_dialog.dart';
 
 var client = Client('http://localhost:8080/')
   ..connectivityMonitor = FlutterConnectivityMonitor();
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Serverpod Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
       home: const MyHomePage(title: 'Serverpod Example'),
     );
@@ -34,98 +35,85 @@ class MyHomePage extends StatefulWidget {
 }
 
 class MyHomePageState extends State<MyHomePage> {
-  // These fields hold the last result or error message that we've received from
-  // the server or null if no result exists yet.
-  String? _resultMessage;
-  String? _errorMessage;
+  List<Post>? _posts;
+  Exception? _connectionException;
 
-  final _textEditingController = TextEditingController();
+  void _connectionFailed(dynamic exception) {
+    setState(() {
+      _posts = null;
+      _connectionException = exception;
+    });
+  }
 
-  // Calls the `hello` method of the `example` endpoint. Will set either the
-  // `_resultMessage` or `_errorMessage` field, depending on if the call
-  // is successful.
-  void _callHello() async {
+  Future<void> _loadPosts() async {
     try {
-      final result = await client.example.hello(_textEditingController.text);
+      final posts = await client.post.getPosts();
       setState(() {
-        _resultMessage = result;
+        _posts = posts;
       });
     } catch (e) {
-      setState(() {
-        _errorMessage = '$e';
-      });
+      _connectionFailed(e);
     }
+  }
+
+  Future<void> _createPost(Post post) async{
+    try{
+      await client.post.createPost(post);
+      await _loadPosts(); 
+    }
+    catch(e){
+      _connectionFailed(e);
+    }
+  }
+
+  void initState() {
+    super.initState();
+    _loadPosts();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: TextField(
-                controller: _textEditingController,
-                decoration: const InputDecoration(
-                  hintText: 'Enter your name',
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: ElevatedButton(
-                onPressed: _callHello,
-                child: const Text('Send to Server'),
-              ),
-            ),
-            _ResultDisplay(
-              resultMessage: _resultMessage,
-              errorMessage: _errorMessage,
-            ),
-          ],
+        title: Text(
+          widget.title,
         ),
       ),
-    );
-  }
-}
+      body: _posts == null
+          ? Container()
+          : ListView.builder(
+              itemCount: _posts!.length,
+              itemBuilder: ((context, index) {
+                return ListTile(
+                  title: Text(_posts![index].caption),
+                );
+              }),
+            ),
+          floatingActionButton: _posts == null
+      ? null
+      : FloatingActionButton(
+          onPressed: () {
+            showPostDialog(
+              context: context,
+              onSaved: (text) {
+                var post = Post(
+                  caption: text,
+                  type: PostType.text,
+                  audience: PostAudienceSettings.everyone,
+                  reply: PostReplySettings.everyone,
+                  userId: 6,
+                  username: "tugberk6",
+                  imgUrl: "https://source.unsplash.com/random/?city,night",
+                  profilePicUrl: "https://source.unsplash.com/random/?people",
+                );
+                _posts!.add(post);
 
-// _ResultDisplays shows the result of the call. Either the returned result from
-// the `example.hello` endpoint method or an error message.
-class _ResultDisplay extends StatelessWidget {
-  final String? resultMessage;
-  final String? errorMessage;
-
-  const _ResultDisplay({
-    this.resultMessage,
-    this.errorMessage,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    String text;
-    Color backgroundColor;
-    if (errorMessage != null) {
-      backgroundColor = Colors.red[300]!;
-      text = errorMessage!;
-    } else if (resultMessage != null) {
-      backgroundColor = Colors.green[300]!;
-      text = resultMessage!;
-    } else {
-      backgroundColor = Colors.grey[300]!;
-      text = 'No server response yet.';
-    }
-
-    return Container(
-      height: 50,
-      color: backgroundColor,
-      child: Center(
-        child: Text(text),
-      ),
+                _createPost(post);
+              },
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
     );
   }
 }
